2d86ed37a0fc4b37e62a3051c1f4c5a78c0bddfd
AppleEducate <rody.davis.jr@gmail.com>
2018-07-12 18:33:13 -0400

    New Changes

--- lib/main.dart
+++ lib/main.dart
@@ -26,76 +26,44 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
+class CustomItem {
+  final String title;
+  final String subtitle;
+  final String id;
+  CustomItem({this.title, this.subtitle, this.id});
+}
+
 class _MyHomePageState extends State<MyHomePage> {
-  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
-  ListModel<int> _list;
-  int _selectedItem;
-  int _nextItem; // The next item inserted when the user presses the '+' button.
 
   @override
   void initState() {
     super.initState();
-    _list = ListModel<int>(
-      listKey: _listKey,
-      initialItems: <int>[0, 1, 2],
-      removedItemBuilder: _buildRemovedItem,
-    );
-    _nextItem = 3;
+
   }
 
-  // Used to build list items that haven't been removed.
-  Widget _buildItem(
-      BuildContext context, int index, Animation<double> animation) {
-    return CardItem(
-      animation: animation,
-      item: _list[index],
-      selected: _selectedItem == _list[index],
-      title: Text('This is the Title'),
-      subtitle: Text('This is the Subtitle'),
-      onTap: () {
-        setState(() {
-          _selectedItem = _selectedItem == _list[index] ? null : _list[index];
+  List<CustomItem> _items = [];
+
+  Widget _buildItem(BuildContext context, int index) {
+    return ListTile(
+      leading: Icon(Icons.info),
+      title: Text(_items[index].title),
+      subtitle: Text(_items[index].subtitle),
+      trailing: IconButton(
+        icon: Icon(Icons.radio_button_unchecked),
+        onPressed: () {
+          setState(() {
+            _items.removeAt(index);
+          });
+        },
+      ),
+    );
+  }
+
+  void _addItem() {
+    setState(() {
+          _items.add(CustomItem(title: '${_items.length}. Test'), );
         });
-      },
-      onDelete: () {
-        setState(() {
-          _list.removeAt(index);
-        });
-      },
-    );
   }
-
-  // Used to build an item after it has been removed from the list. This method is
-  // needed because a removed item remains  visible until its animation has
-  // completed (even though it's gone as far this ListModel is concerned).
-  // The widget will be used by the [AnimatedListState.removeItem] method's
-  // [AnimatedListRemovedItemBuilder] parameter.
-  Widget _buildRemovedItem(
-      int item, BuildContext context, Animation<double> animation) {
-    return CardItem(
-      animation: animation,
-      item: item,
-      selected: false,
-      // No gesture detector here: we don't want removed items to be interactive.
-    );
-  }
-
-  // Insert the "next item" into the list model.
-  void _insert() {
-    // final int index =
-    // _selectedItem == null ? _list.length : _list.indexOf(_selectedItem);
-    _list.insert(_list.length == null ? 0 : _list.length, _list.length + 1);
-  }
-
-  // Remove the selected item from the list model.
-  // void _remove() {
-  //   if (_selectedItem != null) {
-  //     _list.removeAt(_list.indexOf(_selectedItem));
-  //     setState(() {
-  //       _selectedItem = null;
-  //     });
-  //   }
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -105,10 +73,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(0.0),
-        child: AnimatedList(
-          key: _listKey,
-          initialItemCount: _list.length,
+        child: ListView.builder(
           itemBuilder: _buildItem,
+          itemCount: _items.length == null ? 0 : _items.length,
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
@@ -116,7 +83,8 @@ class _MyHomePageState extends State<MyHomePage> {
         icon: const Icon(Icons.add),
         label: const Text('Add a task'),
         onPressed: () {
-          _insert();
+
+          // _insert();
           // showModalBottomSheet<void>(
           //     context: context,
           //     builder: (BuildContext context) {
@@ -156,123 +124,6 @@ class _MyHomePageState extends State<MyHomePage> {
               onPressed: () {},
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-/// Keeps a Dart List in sync with an AnimatedList.
-///
-/// The [insert] and [removeAt] methods apply to both the internal list and the
-/// animated list that belongs to [listKey].
-///
-/// This class only exposes as much of the Dart List API as is needed by the
-/// sample app. More list methods are easily added, however methods that mutate the
-/// list must make the same changes to the animated list in terms of
-/// [AnimatedListState.insertItem] and [AnimatedList.removeItem].
-///
-class ListModel<E> {
-  ListModel({
-    @required this.listKey,
-    @required this.removedItemBuilder,
-    Iterable<E> initialItems,
-  })  : assert(listKey != null),
-        assert(removedItemBuilder != null),
-        _items = List<E>.from(initialItems ?? <E>[]);
-
-  final GlobalKey<AnimatedListState> listKey;
-  final dynamic removedItemBuilder;
-  final List<E> _items;
-
-  AnimatedListState get _animatedList => listKey.currentState;
-
-  void insert(int index, E item) {
-    _items.insert(index, item);
-    _animatedList.insertItem(index);
-  }
-
-  E removeAt(int index) {
-    final E removedItem = _items.removeAt(index);
-    if (removedItem != null) {
-      _animatedList.removeItem(index,
-          (BuildContext context, Animation<double> animation) {
-        return removedItemBuilder(removedItem, context, animation);
-      });
-    }
-    return removedItem;
-  }
-
-  int get length => _items.length;
-
-  E operator [](int index) => _items[index];
-
-  int indexOf(E item) => _items.indexOf(item);
-}
-
-/// Displays its integer item as 'item N' on a Card whose color is based on
-/// the item's value. The text is displayed in bright green if selected is true.
-/// This widget's height is based on the animation parameter, it varies
-/// from 0 to 128 as the animation varies from 0.0 to 1.0.
-class CardItem extends StatelessWidget {
-  const CardItem(
-      {Key key,
-      @required this.animation,
-      this.onTap,
-      @required this.item,
-      this.onDelete,
-      this.subtitle,
-      this.title,
-      this.selected: false})
-      : assert(animation != null),
-        assert(item != null && item >= 0),
-        assert(selected != null),
-        super(key: key);
-
-  final Animation<double> animation;
-  final VoidCallback onTap;
-  final VoidCallback onDelete;
-
-  final int item;
-  final bool selected;
-  final Widget title;
-  final Widget subtitle;
-
-  @override
-  Widget build(BuildContext context) {
-    TextStyle textStyle = Theme.of(context).textTheme.display1;
-    if (selected)
-      textStyle = textStyle.copyWith(color: Colors.lightGreenAccent[400]);
-    return Padding(
-      padding: const EdgeInsets.all(2.0),
-      child: SizeTransition(
-        axis: Axis.vertical,
-        sizeFactor: animation,
-        child: GestureDetector(
-          behavior: HitTestBehavior.opaque,
-          onTap: onTap,
-          child: SizedBox(
-            // height: 128.0,
-            child: Card(
-              // color: Colors.primaries[item % Colors.primaries.length],
-              child: ListTile(
-                leading: Text(
-                  '$item',
-                  style: TextStyle(
-                      color: Colors.primaries[item % Colors.primaries.length]),
-                ),
-                title: title,
-                subtitle: subtitle,
-                trailing: IconButton(
-                  icon: Icon(
-                    Icons.radio_button_unchecked,
-                    size: 30.0,
-                  ),
-                  onPressed: onDelete,
-                ),
-              ),
-            ),
-          ),
         ),
       ),
     );
